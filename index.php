e48cd7654803129590c92f5feb5627731bf24071
进恩哥资源仓库 <119514614+jinenge@users.noreply.github.com>
2025-01-29 22:21:29 +0800

    Delete index.php
    Signed-off-by: 进恩哥资源仓库 <119514614+jinenge@users.noreply.github.com>

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$img_array = glob('bzhi/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
-if(count($img_array) == 0) die('没找到图片文件。请先上传一些图片到 '.dirname(__FILE__).'/bzhi/ 文件夹');
-header('Content-Type: image/png');
-echo(file_get_contents($img_array[array_rand($img_array)]));
-?>
